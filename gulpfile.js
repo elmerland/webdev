a7ddbec3eb58522c1d54c13181cc6322cd3b625f
Elmer Landaverde <elmer@vt.edu>
2014-10-01 23:22:42 -0400

    Add autoprefixer plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./css'));
 });
 
