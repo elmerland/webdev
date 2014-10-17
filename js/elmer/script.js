036d539a5e6bc7e9fe77147f20d0d9a1f6e8cb90
Elmer Landaverde <elmer@vt.edu>
2014-10-17 02:47:46 -0400

    Update my profile

--- js/elmer/script.js
+++ js/elmer/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  console.log("Hello world");
-  $('header h1').fitText(.9);
-  $('header h2').fitText(1.5);
+  // Add responsive font resizing to page title and subtitle
+  // Use lettering to split words into span elements
+  $('header h1, footer h1').fitText(1.1);
+  $('header h2').fitText(2.5).lettering('words');
 });
