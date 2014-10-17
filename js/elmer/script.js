46065c719fc2f258bcdc5ff1ee7c7258bf70f549
Elmer Landaverde <elmer@vt.edu>
2014-10-17 13:37:13 -0400

    Remove subtitle and add inset shadow to footer

--- js/elmer/script.js
+++ js/elmer/script.js
@@ -2,5 +2,4 @@ $(document).ready(function () {
   // Add responsive font resizing to page title and subtitle
   // Use lettering to split words into span elements
   $('header h1, footer h1').fitText(1.1);
-  $('header h2').fitText(2.5).lettering('words');
 });
