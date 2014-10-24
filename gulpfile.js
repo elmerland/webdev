ecffd4c9da0dc1a3feb7cf28ecc3455a00edbb4f
Elmer Landaverde <elmer@vt.edu>
2014-10-24 17:07:48 -0400

    Modify gulp to do autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-
 var stylesheet_files = ['./sass/**/*.scss', './sass/**/*.css'];
 
-gulp.task('sass', function() {
-  return gulp.src(stylesheet_files)
-    .pipe(sass({errLogToConsole: true}))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./css'));
-});
+gulp.task('default', ['watch']);
 
+// Watcher: Will watch for changes in the sass directory and compile
 gulp.task('watch', ['sass'], function() {
   gulp.watch(stylesheet_files, ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+// Sass: Will compile Sass into CSS
+gulp.task('sass', function() {
+  return gulp.src(stylesheet_files)
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: true
+    }))
+    .pipe(gulp.dest('./css'));
+});
