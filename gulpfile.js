36fb9015ec83125d2495f82170e13bd9b32e74fe
Elmer Landaverde <elmer@vt.edu>
2014-10-26 21:01:04 -0400

    Add clean task and ensure serial running of tasks
    The changes also send all build files to the 'dist' folder. By running
    the default task gulp will first clean the distribution files, then
    compile fils and the finally watch for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,80 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var stylesheet_files = ['./sass/**/*.scss', './sass/**/*.css'];
+var fileinclude = require('gulp-file-include');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('default', ['watch']);
-
-// Watcher: Will watch for changes in the sass directory and compile
-gulp.task('watch', ['sass'], function() {
-  gulp.watch(stylesheet_files, ['sass']);
+gulp.task('default', function(callback) {
+  runSequence('clean', 'compile', 'watch', callback);
 });
+
+gulp.task('clean', function (callback) {
+  del('./dist/**', callback);
+});
+
+gulp.task('compile', function(callback) {
+  runSequence(['sass', 'move_js', 'move_images'], 'html', callback);
+});
+
+gulp.task('watch', function(callback) {
+  runSequence(['watch_sass', 'watch_html', 'watch_js', 'watch_images'], callback);
+});
+
+// Watchers --------------------------------------------------
+
+// Will watch for changes in the sass directory and compile
+gulp.task('watch_sass', function() {
+  return gulp.watch('./sass/**', ['sass']);
+});
+
+// Will watch for changes in the html directory and compile
+gulp.task('watch_html', function() {
+  return gulp.watch('./html/**', ['html']);
+});
+
+// Will watch for changes in the js directory
+gulp.task('watch_js', function() {
+  return gulp.watch('./js/**', ['move_js']);
+});
+
+// Will watch for changes in the js directory
+gulp.task('watch_images', function() {
+  return gulp.watch('./images/**', ['move_images']);
+});
+
+// Compilers --------------------------------------------------
 
 // Sass: Will compile Sass into CSS
 gulp.task('sass', function() {
-  return gulp.src(stylesheet_files)
+  return gulp.src(['./sass/**/*.scss', './sass/**/*.css'])
     .pipe(sass({errLogToConsole: true}))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: true
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('html', function() {
+  return gulp.src(['./html/**/*.html', '!./html/**/_*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+// File movers --------------------------------------------------
+
+// Move JavaScript files into build directory
+gulp.task('move_js', function() {
+  return gulp.src('./js/**/*.js')
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+// Move image files into build directory
+gulp.task('move_images', function() {
+  return gulp.src('./images/**')
+    .pipe(gulp.dest('./dist/images'));
 });
