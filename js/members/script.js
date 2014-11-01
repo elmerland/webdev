08fb5562a29eb9dc70363c39562026d37bbe1000
Elmer Landaverde <elmer@vt.edu>
2014-11-01 12:01:56 -0400

    Add text resizing to members page title

--- js/members/script.js
+++ js/members/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  // Resize page title
+  $('.page-content-title h1').fitText(1.5);
   // Resize member card title
   $('.member-card .card-title').fitText(.9);
   // Mouse over card listener
